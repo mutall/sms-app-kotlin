0d3e5ad025f4d374a023a7f21c8a928a5a1d4ec8
amshelhack3r <amshelhack3r@gmail.com>
2019-02-12 18:05:34 +0300

    Moved to folders

---
+++ app/src/main/java/com/hack3r/amshel/mutall/models/ErrorObject.kt
@@ -0,0 +1,10 @@
+package com.hack3r.amshel.mutall.models
+
+
+/**
+ * Create an error object That accepts two parameters as a constructor
+ * The message will be the cause and the code will be used for filtering the array
+ */
+class ErrorObject (var message: String, var code: Int){
+
+}
