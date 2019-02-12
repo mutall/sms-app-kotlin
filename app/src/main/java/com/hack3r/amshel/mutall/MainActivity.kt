e452b4acca2b1e033e298841ca06d668241e7eb6
amshelhack3r <amshelhack3r@gmail.com>
2019-02-12 14:26:56 +0300

    MainActivity file

--- app/src/main/java/com/hack3r/amshel/mutall/MainActivity.kt
+++ app/src/main/java/com/hack3r/amshel/mutall/MainActivity.kt
@@ -2,11 +2,18 @@ package com.hack3r.amshel.mutall
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.hack3r.amshel.mutall.helpers.Library
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val library = Library(this);
+
+        retrieveAccounts.setOnClickListener {
+
+        }
     }
 }
