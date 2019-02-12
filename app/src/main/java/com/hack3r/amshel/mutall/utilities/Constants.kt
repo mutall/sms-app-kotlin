48945c5375ff1b09a337c50faa430455209eb105
amshelhack3r <amshelhack3r@gmail.com>
2019-02-12 18:01:39 +0300

    global constants

---
+++ app/src/main/java/com/hack3r/amshel/mutall/utilities/Constants.kt
@@ -0,0 +1,6 @@
+package com.hack3r.amshel.mutall.utilities
+
+
+const val TAG = "debug"
+const val ACCOUNTS_URL = "http://mutall.co.ke/android/details/client"
+const val NOREADING_URL = "http://mutall.co.ke/android/details/client"
