9540d284f708e35bdd8f40a133b113d105314703
amshelhack3r <amshelhack3r@gmail.com>
2019-02-20 11:35:42 +0300

    Add webview

---
+++ app/src/main/java/com/hack3r/amshel/mutall/controllers/WebView.kt
@@ -0,0 +1,23 @@
+package com.hack3r.amshel.mutall.controllers
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Base64
+import com.hack3r.amshel.mutall.R
+import com.hack3r.amshel.mutall.utilities.RESPONSE
+import kotlinx.android.synthetic.main.activity_webview.*
+
+
+class WebView:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_webview)
+
+        if(intent.hasExtra(RESPONSE)) {
+            var response = intent.getStringExtra(RESPONSE)
+            val encodedHtml = Base64.encodeToString(response.toByteArray(), Base64.NO_PADDING)
+
+            webview.loadData(encodedHtml, "text/html", "base64")
+        }
+    }
+}
