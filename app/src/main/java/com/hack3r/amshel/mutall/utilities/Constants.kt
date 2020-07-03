dab9c5de064d4321117c4f393c255f1361e6cabd
amshelhack3r <amshelhack3r@gmail.com>
2020-07-03 11:47:02 +0300

    modified kplc send_number

--- app/src/main/java/com/hack3r/amshel/mutall/utilities/Constants.kt
+++ app/src/main/java/com/hack3r/amshel/mutall/utilities/Constants.kt
@@ -8,4 +8,4 @@ const val NOREADING_URL = "http://mutall.co.ke/android/details/client"
 const val POST_BILLS = "http://mutall.co.ke/mutall_rental/index.php?class=msg&method=save_all"
 const val TEST_URL = "http://mutall.co.ke/android/post.php"
 const val MUTALL_URL = "http://mutall.co.ke/mutall_data/home.php";
-const val SEND_NUM = "95551"
+const val SEND_NUM = "97771"
